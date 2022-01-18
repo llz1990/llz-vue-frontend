d3e6fddde04448c3539d88f8d19ab3433aebe965
llz1990 <754960880@qq.com>
2022-01-18 22:53:47 +0800

    修改打包路径

--- vue.config.js
+++ vue.config.js
@@ -41,7 +41,7 @@ const cdn = {
 
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/permission/" : "/",
-  outputDir: './dist',
+  outputDir: './llz-vue-dist',
   assetsDir:'static',
   filenameHashing:true, // false 来关闭文件名哈希
   lintOnSave: false, // 关闭eslint
